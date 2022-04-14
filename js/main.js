75e972aeab0d2d0ba64f72ea3d0dfd0430ca6f12
taher salah <tahersalah2016@gmail.com>
2022-04-14 03:51:06 +0200

    add icone and  dynamic value

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const warpper = document.querySelector('.wrapper'),
     infoTxt = inputPart.querySelector('.info-text'),
     inputField = inputPart.querySelector('input'),
     locationBtn = inputPart.querySelector('button')
-
+let tempApi;
 
 
 
@@ -14,24 +14,59 @@ inputField.addEventListener('keyup', e => {
 })
 
 function requestApi(city) {
-    let tempApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3fcf81928648ed81141be245e032222c`
-    fetch(tempApi).then(response => response.json()).then(result => weatherDetails(result))
+    tempApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=3fcf81928648ed81141be245e032222c`
+    fetchData()
 }
 
+function fetchData() {
+    infoTxt.innerText = 'Getting Weather Details';
+    infoTxt.classList.add('pending')
+    fetch(tempApi).then(response => response.json()).then(result => weatherDetails(result))
+
+}
 locationBtn.addEventListener('click', () => {
     if (navigator.geolocation) { //// if browser support location
         navigator.geolocation.getCurrentPosition(onSuccess, onError)
+
     } else(console.error('not define'))
 })
 
 function onSuccess(position) {
-    console.log(position)
+    const { latitude, longitude } = position.coords
+    tempApi = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=3fcf81928648ed81141be245e032222c`;
+    fetchData()
+    console.log(position.weather)
 }
 
 function onError(error) {
-    console.log(error.message)
+    infoTxt.innerText = error.message;
+    infoTxt.classList.add('error');
+
 }
 
 function weatherDetails(info) {
+    infoTxt.classList.replace('pending', 'error')
+
+    if (info.cod == "404") {
+        infoTxt.innerText = `'${inputField.value}' city name not found `;
+    } else {
+        infoTxt.classList.remove('pending', 'error');
+        warpper.classList.add('active')
+
+        //////////// get value by object //////
+        const city = info.name
+        const country = info.sys.country
+        const { description, id } = info.weather[0]
+        const { feels_like, humidity, temp } = info.main
+            /////get elements by html var //////
+
+        warpper.querySelector('.temp .number').innerHTML = Math.floor(temp);
+        warpper.querySelector('.weather').innerHTML = description;
+        warpper.querySelector('.location span').innerHTML = `${country} ${city}`;
+        warpper.querySelector('.details .temp .number').innerHTML = Math.floor(feels_like);
+        warpper.querySelector('.details .temp .number2').innerHTML = humidity;
+
+
+    }
     console.log(info)
 }
